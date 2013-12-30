7c9783ad00ab89ee4523badefe2af5f46439b887
Hung-Yi Chen <gaod.chen@gmail.com>
2013-12-30 22:16:07 +0800

    Fix regex of fetching version

--- lib/NetApp/Filer/Version.pm
+++ lib/NetApp/Filer/Version.pm
@@ -40,7 +40,7 @@ use overload '""' => 'get_string';
         $string_of{$ident}	= $args{string};
 
         $args{string} =~
-            m{ NetApp \s+ Release \s+ (\S+) : \s+ (.*) }gmx ||
+            m{ NetApp \s+ Release \s+ (.+): \s+ (.+) }gmx ||
                    croak ("Invalid version string: $args{string}\n");
 
         $release_of{$ident}	= $1;
